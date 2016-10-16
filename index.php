87a570babb43f6b8075f2bdb716976eb004c9463
Isaac <r.sato7690@me.com>
2016-10-16 18:00:59 +0900

    Fix title

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ $page = $_GET['page'] ?: 1;
 <html lang="ja">
 <head>
   <meta charset="utf-8">
-  <title>jsTest</title>
+  <title>PicColle</title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0">
   <!-- CSS -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootswatch/3.3.7/paper/bootstrap.min.css">
