af85caaea6f78ce4881ff027663876ad4e90dc5a
Isaac <r.sato7690@me.com>
2016-10-24 01:18:11 +0900

    Add fav and hate to localStrage

--- src/script.js
+++ src/script.js
@@ -1,3 +1,8 @@
+storageFlag = false;
+var fav = [];
+var hate = [];
+init();
+
 $(function() {
   $('img.lazy').lazyload({
     threshold: 1000
@@ -5,13 +10,37 @@ $(function() {
 
   //Modal
   $('img.lazy').click(function() {
-    $('#detail img').attr('src', $(this).attr('data-original'));
+    src = $(this).attr('data-original');
+    $('#detail img').attr('src', src);
+    // srotageが使用できない場合、ボタンを使用不可にする
+    if(!storageFlag) {
+      $('#favorite').prop('disabled', true);
+      $('#hate').prop('disabled', true);
+    }
     $('#detail').modal();
   });
   $('#favorite').click(function() {
-    console.log("Fav: " + $('#detail img').attr('src'));
+    console.log("Fav: " + src);
+    localStorage[src] = "fav";
   });
   $('#hate').click(function() {
-    console.log("Hate: " + $('#detail img').attr('src'));
+    console.log("Hate: " + src);
+    localStorage[src] = "hate";
   });
 });
+
+
+function init() {
+  // Check
+  if(('localStorage' in window) && (window.localStorage !== null)) {
+    for(var key in localStorage) {
+      var val = localStorage.getItem(key);
+      if(val == "fav") fav.push(key);
+      else hate.push(key);
+    }
+    storageFlag = true;
+  } else {
+    alert("このブラウザはHTML5 WebStorageApiが使用できないため、お気に入り保存機能が使えません。");
+    storageFlag = false;
+  }
+}
