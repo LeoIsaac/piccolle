a67564e8e18f6baf2f4f86621bf12c8c181ca09c
Isaac <r.sato7690@me.com>
2016-10-30 23:56:17 +0900

    Add ttp protocol's img

--- scraping.php
+++ scraping.php
@@ -19,6 +19,7 @@ class Scraping {
       return $this->typeNormal();
   }
 
+  // URLから2chかそうでないかを判別し、2chならtrue、それ以外ならfalseを返す
   private function is2ch() {
     if(preg_match("/^https?:\/\/(www\.)?(.*\.)?(2ch\.sc|bbspink\.com)(.*)$/", $this->url))
       return true;
@@ -26,23 +27,38 @@ class Scraping {
       return false;
   }
 
+  // 2chだった場合、aタグの.(jpg|png)で終わっているもの全てを配列に入れて返す
   private function type2ch() {
-    $xml = $this->xml($this->url);
-    $imgs = $xml->xpath('//a');
+    $xpath = $this->xpath($this->url);
+    $imgs = $xpath->query('//a');
     foreach($imgs as $img) {
-      $array = (array)$img;
-      $src = $array['@attributes']['href'];
-      if(preg_match("/^https?:\/\/(www\.)?(2ch\.io\/|pinktower\.com\/\?https?:\/\/)?(.*\.(jpg|png))$/", $src, $matches)) {
+      $src = $img->textContent;
+      if(preg_match("/\.(jpg|png)$/", $src)) {
         $this->pic++;
         if( $this->pic <= ($this->page-1) * $this->limit || $this->count >= $this->limit ) continue;
-        $ret[] = "http://" . $matches[3];
+        $ret[] = $src;
         $this->count++;
+      }
+    }
+    // ttpプロトコル対応
+    $msgs = $xpath->query('//div[@class="message"]');
+    foreach($msgs as $msg) {
+      $lines = $msg->textContent;
+      $line = explode(" ", $lines);
+      foreach($line as $src) {
+        if(preg_match("/^ttp.*\.(jpg|png)/", $src)) {
+          $this->pic++;
+          if( $this->count >= $this->limit ) continue;
+          $ret[] = 'h' . $src;
+          //$this->count++;
+        }
       }
     }
     $ret[] = $this->pic;
     return $ret;
   }
 
+  // 2chでない場合、imgタグのsrcを配列に入れて返す
   private function typeNormal() {
     $xml = $this->xml($this->url);
     $imgs = $xml->xpath('//img');
@@ -58,6 +74,12 @@ class Scraping {
     }
     $ret[] = $this->pic;
     return $ret;
+  }
+
+  private function xpath($url) {
+    $dom = new DOMDocument;
+    @$dom->loadHTMLFile($url);
+    return new DOMXPath($dom);
   }
 
   private function xml($url) {
